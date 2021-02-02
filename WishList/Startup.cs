898f9dc4be456b7f2de193ba4d5ea90be51a4792
Jeremy Francisco <jeremy.francisco@cognex.com>
2021-02-02 17:13:32 -0500

    Add ApplicationDbContext.cs with DbSet<Item>   * Add Item with Id and Description properties   * Add DbContext in Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WishList.Data;
 
 namespace WishList
 {
@@ -12,13 +14,15 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseRouting();
-            
+
             app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
 
             if (env.IsDevelopment())
